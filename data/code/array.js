eec819140089c61a0d8897d4101ee06477dce178
tagopi <khoumanitaha23@gmail.com>
2022-10-30 00:17:23 -0400

    still good

--- data/code/array.js
+++ data/code/array.js
@@ -1,54 +1,53 @@
-const FLAGS = [
-    { name: 'Antigua And Barbuda',path: '../images/flags/Antigua_And_Barbuda.png'},
-    { name: 'Armenia', path: ' ../images/flags/Armenia.png' },
-    { name: 'Australia', path: '../images/flags/Australia.png' },
-    { name: 'Azerbaijan', path: '../images/flags/Azerbaijan.png' },
-    { name: 'Barbados', path: '../images/flags/Barbados.png' },
-    { name: 'Belize', path: '../images/flags/Belize.png' },
-    { name: 'Bhutan', path: '../images/flags/Bhutan.png' },
-    { name: 'Brunei', path: '../images/flags/Brunei.png' },
-    { name: 'Cameroon', path: '../images/flags/Cameroon.png' },
-    { name: 'Central African Republic',path: '../images/flags/Central_African_Republic.png'},
-    { name: 'Comoros', path: '../images/flags/Comoros.png' },
-    { name: 'Croatia', path: '../images/flags/Croatia.png' },
-    { name: 'Cyprus', path: '../images/flags/Cyprus.png' },
-    { name: "Côte d'Ivoire",path: "../images/flags/Côte_d'Ivoire.png"},
-    { name: 'Dominica', path: '../images/flags/Dominica.png' },
-    { name: 'Egypt', path: '../images/flags/Egypt.png' },
-    { name: 'Eswatini', path: '../images/flags/Eswatini.png' },
-    { name: 'Fiji', path: '../images/flags/Fiji.png' },
-    { name: 'Grenada', path: '../images/flags/Grenada.png' },
-    { name: 'Guyana', path: '../images/flags/Guyana.png' },
-    { name: 'Indonesia', path: '../images/flags/Indonesia.png' },
-    { name: 'Iraq', path: '../images/flags/Iraq.png' },
-    { name: 'Ireland', path: '../images/flags/Ireland.png' },
-    { name: 'Kazakhstan', path: '../images/flags/Kazakhstan.png' },
-    { name: 'Liechtenstein', path: '../images/flags/Liechtenstein.png' },
-    { name: 'Marshall Islands',path: '../images/flags/Marshall_Islands.png'},
-    { name: 'Monaco', path: '../images/flags/Monaco.png' },
-    { name: 'Montenegro', path: '../images/flags/Montenegro.png' },
-    { name: 'Mozambique', path: '../images/flags/Mozambique.png' },
-    { name: 'New Zealand',path: '../images/flags/New_Zealand.png'},
-    { name: 'Palestine', path: '../images/flags/Palestine.png' },
-    { name: 'Papua New Guinea',path:'../images/flags/Papua_New_Guinea.png'},
-    { name: 'Poland', path: '../images/flags/Poland.png' },
-    { name: 'Saint Kitts and Nevis',path: '../images/flags/Saint_Kitts_and_Nevis.png'},
-    { name: 'Saint Lucia',path: '../images/flags/Saint_Lucia.png'},
-    { name: 'Saint Vincent and the Grenadines',path: '../images/flags/Saint_Vincent_and_the_Grenadines.png'},
-    { name: 'Senegal', path: '../images/flags/Senegal.png' },
-    { name: 'Serbia', path: '../images/flags/Serbia.png' },
-    { name: 'Seychelles', path: '../images/flags/Seychelles.png' },
-    { name: 'Slovakia', path: '../images/flags/Slovakia.png' },
-    { name: 'Slovenia', path: '../images/flags/Slovenia.png' },
-    { name: 'Sudan', path: '../images/flags/Sudan.png' },
-    { name: 'Syria', path: '../images/flags/Syria.png' },
-    { name: 'Tajikistan', path: '../images/flags/Tajikistan.png' },
-    { name: 'Turkmenistan', path: '../images/flags/Turkmenistan.png' },
-    { name: 'Tuvalu', path: '../images/flags/Tuvalu.png' },
-    { name: 'Uganda', path: '../images/flags/Uganda.png' },
-    { name: 'Uzbekistan', path: '../images/flags/Uzbekistan.png' },
-    { name: 'Vatican City',path: '../images/flags/Vatican_City.png'},
-    { name: 'Yemen', path: '../images/flags/Yemen.png' },
-    { name: 'Zambia', path: '../images/flags/Zambia.png' },
-    { name: 'Zimbabwe', path: '../images/flags/Zimbabwe.png' }
+  export let flags = [
+ {nam: 'Antigua And Barbuda', path: 'data/images/flags/Antigua_And_Barbuda.png', usd: 'false'}
+,{nam: 'Armenia', path: ' data/images/flags/Armenia.png', usd: 'false'}
+,{nam: 'Australia', path: 'data/images/flags/Australia.png', usd: 'false'}
+,{nam: 'Azerbaijan', path: 'data/images/flags/Azerbaijan.png', usd: 'false'}
+,{nam: 'Barbados', path: 'data/images/flags/Barbados.png', usd: 'false'}
+,{nam: 'Belize', path: 'data/images/flags/Belize.png', usd: 'false'}
+,{nam: 'Bhutan', path: 'data/images/flags/Bhutan.png', usd: 'false'}
+,{nam: 'Brunei', path: 'data/images/flags/Brunei.png', usd: 'false'}
+,{nam: 'Cameroon', path: 'data/images/flags/Cameroon.png', usd: 'false'}
+,{nam: 'Central African Republic', path: 'data/images/flags/Central_African_Republic.png', usd: 'false'}
+,{nam: 'Comoros', path: 'data/images/flags/Comoros.png', usd: 'false'}
+,{nam: 'Croatia', path: 'data/images/flags/Croatia.png', usd: 'false'}
+,{nam: 'Cyprus', path: 'data/images/flags/Cyprus.png', usd: 'false'}
+,{nam: "Côte d'Ivoire", path: "data/images/flags/Côte_d'Ivoire.png", usd: 'false'}
+,{nam: 'Dominica', path: 'data/images/flags/Dominica.png', usd: 'false'}
+,{nam: 'Egypt', path: 'data/images/flags/Egypt.png', usd: 'false'}
+,{nam: 'Eswatini', path: 'data/images/flags/Eswatini.png', usd: 'false'}
+,{nam: 'Fiji', path: 'data/images/flags/Fiji.png', usd: 'false'}
+,{nam: 'Grenada', path: 'data/images/flags/Grenada.png', usd: 'false'}
+,{nam: 'Guyana', path: 'data/images/flags/Guyana.png', usd: 'false'}
+,{nam: 'Indonesia', path: 'data/images/flags/Indonesia.png', usd: 'false'}
+,{nam: 'Iraq', path: 'data/images/flags/Iraq.png', usd: 'false'}
+,{nam: 'Ireland', path: 'data/images/flags/Ireland.png', usd: 'false'}
+,{nam: 'Kazakhstan', path: 'data/images/flags/Kazakhstan.png', usd: 'false'}
+,{nam: 'Liechtenstein', path: 'data/images/flags/Liechtenstein.png', usd: 'false'}
+,{nam: 'Marshall Islands', path: 'data/images/flags/Marshall_Islands.png', usd: 'false'}
+,{nam: 'Monaco', path: 'data/images/flags/Monaco.png', usd: 'false'}
+,{nam: 'Montenegro', path: 'data/images/flags/Montenegro.png', usd: 'false'}
+,{nam: 'Mozambique', path: 'data/images/flags/Mozambique.png', usd: 'false'}
+,{nam: 'New Zealand', path: 'data/images/flags/New_Zealand.png', usd: 'false'}
+,{nam: 'Palestine', path: 'data/images/flags/Palestine.png', usd: 'false'}
+,{nam: 'Papua New Guinea', path: 'data/images/flags/Papua_New_Guinea.png', usd: 'false'}
+,{nam: 'Poland', path: 'data/images/flags/Poland.png', usd: 'false'}
+,{nam: 'Saint Kitts and Nevis', path: 'data/images/flags/Saint_Kitts_and_Nevis.png', usd: 'false'}
+,{nam: 'Saint Lucia', path: 'data/images/flags/Saint_Lucia.png', usd: 'false'}
+,{nam: 'Saint Vincent and the Grenadines', path: 'data/images/flags/Saint_Vincent_and_the_Grenadines.png', usd: 'false'}
+,{nam: 'Senegal', path: 'data/images/flags/Senegal.png', usd: 'false'}
+,{nam: 'Serbia', path: 'data/images/flags/Serbia.png', usd: 'false'}
+,{nam: 'Seychelles', path: 'data/images/flags/Seychelles.png', usd: 'false'}
+,{nam: 'Slovakia', path: 'data/images/flags/Slovakia.png', usd: 'false'}
+,{nam: 'Slovenia', path: 'data/images/flags/Slovenia.png', usd: 'false'}
+,{nam: 'Sudan', path: 'data/images/flags/Sudan.png', usd: 'false'}
+,{nam: 'Syria', path: 'data/images/flags/Syria.png', usd: 'false'}
+,{nam: 'Tajikistan', path: 'data/images/flags/Tajikistan.png', usd: 'false'}
+,{nam: 'Turkmenistan', path: 'data/images/flags/Turkmenistan.png', usd: 'false'}
+,{nam: 'Tuvalu', path: 'data/images/flags/Tuvalu.png', usd: 'false'}
+,{nam: 'Uganda', path: 'data/images/flags/Uganda.png', usd: 'false'}
+,{nam: 'Uzbekistan', path: 'data/images/flags/Uzbekistan.png', usd: 'false'}
+,{nam: 'Yemen', path: 'data/images/flags/Yemen.png', usd: 'false'}
+,{nam: 'Zambia', path: 'data/images/flags/Zambia.png', usd: 'false'}
+,{nam: 'Zimbabwe', path: 'data/images/flags/Zimbabwe.png', usd: 'false'}
   ]
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//needed DOM variables
-const body = document.querySelector("body")
+import { flags } from "./data/code/array.js"
+//VARIABLES-NEDEED----------------------------------------------------------------------------------------------------------
     //landing page variables
     const landingPage = document.querySelector("#landing-page")
     const landingPagePlayer1 = document.querySelector("#player1")
@@ -18,7 +18,28 @@ const body = document.querySelector("body")
     const continu = document.querySelector("#continue")
     const mainMenu = document.querySelector("#main-menu")
     const reset = document.querySelector("#reset")
-//needed functions
+    //game vars
+    let idys = []
+    const container = document.querySelector("#quiz-f")
+    let used = []
+    const quiz = document.querySelector("#quiz")
+    let  timerHtml = document.querySelector("#timer")
+    let isThat;
+//WEIRD PARTS--------------------------------------------------------------------------------------------------------------
+let save = []
+let num ;
+function randomBeta(){ 
+    return(Math.floor(Math.random()*51))
+}
+save.push(randomBeta())
+num= save[0]
+function newNum(){
+    save = []
+    save.push(Math.floor(Math.random()*51))
+    num= save[0]
+}
+//00
+//FUNCTIONS NEDEED------------------------------------------------------------------------------------------------------------
 function move(a,b){
     b.forEach( s => {
         s.classList.remove("out")
@@ -33,37 +54,165 @@ function up(a){
 function down (a){
     a.style.zIndex = 0;
 }
-//moving to level
-    //landing page event listeners
-    landingPagePlayer1.addEventListener("click",function(){
-        move([landingPage],[level])
+function ranid(arr){
+    return arr[Math.floor(Math.random()*arr.length)]
+}
+function whatIs(){
+    let namo = document.querySelector(`#${ranid(idys)}`)
+    isThat = document.createElement("p")
+    isThat.innerText = namo.innerText
+    isThat.classList.add("question")
+    isThat.setAttribute("id",`${namo.getAttribute("id")}`)
+    container.append(isThat)
+}
+function isnot(n){
+    let is = used.every(not =>{
+       return not !== n
     })
-    //level event listeners
-    levelReturn.addEventListener("click",function(e){
-        move([level],[landingPage])
-    })
-    easy.addEventListener("click",function(e){
-        move([level],[game,menuPopUp])
-    })
-    normal.addEventListener("click",function(e){
-        move([level],[game,menuPopUp])
-    })
-    hard.addEventListener("click",function(e){
-        move([level],[game,menuPopUp])
-    })
-    //game event listener
-    menuIcon.addEventListener("click",function(e){
-        up(menuPopUp)
-    })
-    //menuPopup event listeners
-    continu.addEventListener("click",function(e){
-        down(menuPopUp)
-    })
-    reset.addEventListener("click",function(){
-        down(menuPopUp)
-    })
-    mainMenu.addEventListener("click",function(e){
-        down(menuPopUp)
-        move([game,menuPopUp],[landingPage])
-    })
+    return is
+}
+function random(){
+    if(used.length === 51){
+        used = []
+        newNum()
+        used.push(num)
+        return num
+    }
+    else{
+        while(isnot(num) === false){
+            newNum()
+        }
+        used.push(num)
+        return num
+    }
 
+}
+function pathOf(n){
+    return flags[n].path
+}
+function nameOf(n){
+    return flags[n].nam
+}
+function provide(nam){
+    let bam = []
+    for(let i=0;i<nam;i++){
+        bam.push(random())
+    }
+    return bam
+}
+function makeFlag(n){
+    let img = document.createElement("img")
+    img.setAttribute("id",`i${n}`)
+    img.setAttribute("src",pathOf(n))
+    img.setAttribute("alt",nameOf(n))
+    img.classList.add("flag")
+    return img
+}
+function makeName(n){
+    let name = document.createElement("p")
+    name.innerText = nameOf(n)
+    name.classList.add("c-name")
+    name.setAttribute("id",`n${n}`)
+    idys.push(`n${n}`)
+    return name
+}
+function makeCountry(n){
+    let div = document.createElement("div")
+    div.classList.add("country")
+    div.append(makeFlag(n),makeName(n))
+    return div
+}
+function display(arr){
+    clear()
+    idys=[]
+    arr.forEach((num,ind)=>{
+        quiz.append(makeCountry(num))
+    })
+}
+function clear(){
+    Array.from(quiz.children).forEach((country,ind) =>{
+        quiz.removeChild(country)
+    })
+}
+function timer(s,func){
+    let t = (s*1000)+1000
+    let to = new Date().getTime()+t;
+    let timerJs = setInterval(function(){
+        var now = new Date().getTime()
+        var timeLeft = Math.floor((to-now)/1000);
+        timerHtml.innerText=`${timeLeft}`
+        if(timeLeft<0){
+            clearInterval(timerJs)
+            timerHtml.innerText=""
+            func()
+        }
+    },100)
+}
+function hide(){
+    let pes = Array.from(document.querySelectorAll(".c-name"))
+    pes.forEach(a=>{
+        a.style.display = "none"
+    })
+}
+function yes(el){
+    el.parentElement.classList.add("yes")
+}
+function no(el){
+    el.parentElement.classList.add("no")
+    let dr = document.querySelector(`#i${isThat.getAttribute("id")[1]}`)
+    console.log(dr)
+    dr.classList.add("yes")
+    
+}
+function test(img){
+    if(img.getAttribute("id")[1] === isThat.getAttribute("id")[1]){
+        yes(img)
+    }
+    else{
+        no(img)
+    }
+}
+function testTime(){
+    hide()
+    whatIs()
+    let ces = Array.from(document.querySelectorAll(".flag"))
+    ces.forEach(a =>{
+        a.addEventListener("click",function(e){test(e.target)})
+    })
+}
+//EVENT LISTENERS---------------------------------------------------------------------------------------------------
+//landing page event listeners
+landingPagePlayer1.addEventListener("click",function(){
+    move([landingPage],[level])
+})
+//level event listeners
+levelReturn.addEventListener("click",function(e){
+    move([level],[landingPage])
+})
+normal.addEventListener("click",function(e){
+    move([level],[game,menuPopUp])
+})
+easy.addEventListener("click",function(e){
+    move([level],[game,menuPopUp])
+    timer(5,testTime)
+})
+hard.addEventListener("click",function(e){
+    move([level],[game,menuPopUp])
+})
+//game event listener
+menuIcon.addEventListener("click",function(e){
+    up(menuPopUp)
+})
+//menuPopup event listeners
+continu.addEventListener("click",function(e){
+    down(menuPopUp)
+})
+reset.addEventListener("click",function(){
+    down(menuPopUp)
+})
+mainMenu.addEventListener("click",function(e){
+    down(menuPopUp)
+    move([game,menuPopUp],[landingPage])
+})
+//EXECUTIONS----------------------------------------------------------------------------------------------------------
+display(provide(3))
